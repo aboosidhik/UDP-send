0340f2586933f297552f0ff6ff1e2a0eb5c7493c
Learner <aboosidhik@gmail.com>
2017-05-26 19:06:56 +0200

    Update tp_bntp.cpp

--- tp_bntp.cpp
+++ tp_bntp.cpp
@@ -1,4 +1,4 @@
-
+//g++ tp_bntp.cpp -I/usr/local/include -L/usr/local/lib -lavcodec -lavformat -lswscale -lavfilter -lavutil -lx264 -lz -lm -lopencv_imgproc -lopencv_highgui -lopencv_core `pkg-config --cflags --libs opencv` `pkg-config --cflags --libs glib-2.0` -lm -lvpx -ljansson -std=gnu++11
 /*! \file    audio-mixing.c
  * \author   Aboobeker Sidhik <aboosidhik@gmail.com>
  * \copyright GNU General Public License v3
