063c529c9ec989f7a8f347eb663f00ee150fbafb
Learner <aboosidhik@gmail.com>
2017-06-08 12:43:39 +0200

    Update relay.sh

--- relay.sh
+++ relay.sh
@@ -2,3 +2,4 @@
 apt-get update
 apt-get install libevent-dev
 apt-get install ffmpeg
+./configure   --enable-gpl   --enable-shared    --enable-nonfree  --enable-libass   --enable-libfdk-aac   --enable-libfreetype   --enable-libmp3lame   --enable-libopus   --enable-libtheora   --enable-libvorbis   --enable-libvpx   --enable-libx264   --enable-libx265 --cc="gcc -m64 -fPIC"
