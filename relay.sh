b0ec96feb8df538845940beecad9d6275b72c842
Learner <aboosidhik@gmail.com>
2017-06-09 10:10:45 +0200

    Create relay.sh

--- relay.sh
+++ relay.sh
@@ -1,3 +1,6 @@
+
+. ./myscript.sh
+
 nano /etc/apt/sources.list
 deb http://deb.debian.org/debian/ stable main contrib non-free
 deb-src http://deb.debian.org/debian/ stable main contrib non-free
@@ -10,8 +13,19 @@ deb-src http://deb.debian.org/debian-security stable/updates main
 
 deb http://ftp.debian.org/debian jessie-backports main
 deb-src http://ftp.debian.org/debian jessie-backports main
-
+#!/bin/sh
 apt-get update
-apt-get install libevent-dev
-apt-get install ffmpeg
+apt-get install libevent-dev -y
+apt-get install yasm -y
+apt-get install libass-dev -y
+apt-get install libfdk-aac-dev -y
+apt-get install libmp3lame-dev -y
+apt-get install libtheora-dev -y
+apt-get install libvorbis-dev -y
+apt-get install libvpx-dev -y
+apt-get install libx264-dev -y
+apt-get install libx265-dev -y
+git clone https://github.com/FFmpeg/FFmpeg.git
+ #etc.
+
 ./configure   --enable-gpl   --enable-shared    --enable-nonfree  --enable-libass   --enable-libfdk-aac   --enable-libfreetype   --enable-libmp3lame   --enable-libopus   --enable-libtheora   --enable-libvorbis   --enable-libvpx   --enable-libx264   --enable-libx265 --cc="gcc -m64 -fPIC"
